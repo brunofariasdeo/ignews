4f2c63f854e95d4079e2b32bc13b100447bc3018
brunofariasdeo <jfarias@daitan.com>
2022-01-07 09:28:04 -0300

    Create Header and SignInButton components

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,28 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button 
+      className={styles.signInButton}
+      type="button"
+    >
+      <FaGithub color="#04d361"/>
+      Bruno Oliveira
+      <FiX className={styles.closeIcon} color="#737380"/>
+    </button>
+  ) : (
+    <button 
+      className={styles.signInButton}
+      type="button"
+    >
+      <FaGithub color="#eba417"/>
+      Sign in with Github
+    </button>
+  )
+  ;
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,300&display=swap" rel="stylesheet"/>
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
