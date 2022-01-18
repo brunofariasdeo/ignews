7a6f6d8cb4b61abf2909f96f7020b1651f333a04
brunofariasdeo <jfarias@daitan.com>
2022-01-18 19:26:03 -0300

    Generate a checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,9 +6,19 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data, status } = useSession();
+
+  function handleSubscribe() {
+    if (status !== 'authenticated'){
+      signIn('github');
+      return;
+    }
+  }
+
   return (
     <button
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
       type="button"
     >
       Subscribe now
