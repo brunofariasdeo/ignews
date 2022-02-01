ae5ad27629c20a2ffb0ae397c4a07bfeda3f150d
brunofariasdeo <jfarias@daitan.com>
2022-02-01 10:24:36 -0300

    Redirect to Stripe on subscribe click

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,10 +10,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data, status } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (status !== 'authenticated'){
       signIn('github');
       return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId: sessionId});
+    } 
+    catch (err) {
+      console.log(err.message);
+      alert(err.message);
     }
   }
 
